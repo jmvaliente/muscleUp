3ddb074b7f24d20ccd9a63ba44ec770134221160
jmvaliente <valiente_jose2@hotmail.com>
2022-11-03 18:50:33 +0100

    update register form

--- muscleUp/src/App.jsx
+++ muscleUp/src/App.jsx
@@ -1,12 +1,29 @@
 import React from 'react'
 import Navbar from './ui/components/Navbar'
 import Card from './ui/components/Card'
+import Register from './ui/components/Register'
+import supabase from './services/supabase'
+
+import useFetchData from './hooks/useFetchData'
+import useGetUser from './hooks/useGetUser'
 
 function App() {
+  const user = useGetUser()
+  const exercises = useFetchData()
+
   return (
     <div>
       <Navbar />
-      <Card />
+      {user.data.data?.user ? (
+        <div className="flex flex-wrap justify-center">
+          {exercises.data.map(exercise => (
+            <Card key={exercise.id}>{exercise}</Card>
+          ))}
+        </div>
+      ) : (
+        <Register />
+      )}
+      <button onClick={() => supabase.auth.signOut()}>Log out</button>
     </div>
   )
 }
--- muscleUp/src/ui/components/Register/View.jsx
+++ muscleUp/src/ui/components/Register/View.jsx
@@ -1,6 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import supabase from '../../../services/supabase'
 
 const Register = () => {
+  const [formState, setFormState] = useState({
+    email: '',
+    password: '',
+  })
+
+  const navigate = useNavigate()
+
+  const handleClickSend = async event => {
+    event.preventDefault()
+    const { error } = await supabase.auth.signInWithPassword({
+      email: formState.email,
+      password: formState.password,
+    })
+    if (error) {
+      console.log(error)
+    } else {
+      navigate('/')
+    }
+  }
   return (
     <section className="h-screen">
       <div className="container px-6 py-12 h-full">
@@ -19,6 +40,9 @@ const Register = () => {
                   type="text"
                   className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                   placeholder="Email address"
+                  onChange={event =>
+                    setFormState({ ...formState, email: event.target.value })
+                  }
                 />
               </div>
 
@@ -27,6 +51,9 @@ const Register = () => {
                   type="password"
                   className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                   placeholder="Password"
+                  onChange={event =>
+                    setFormState({ ...formState, password: event.target.value })
+                  }
                 />
               </div>
 
@@ -58,6 +85,7 @@ const Register = () => {
                 className="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out w-full"
                 data-mdb-ripple="true"
                 data-mdb-ripple-color="light"
+                onClick={event => handleClickSend(event)}
               >
                 Sign in
               </button>
