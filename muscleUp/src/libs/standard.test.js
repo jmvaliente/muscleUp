ed102623b47725e62edcef41644975ce64b4561d
jmvaliente <valiente_jose2@hotmail.com>
2023-02-24 00:59:12 +0100

    add new tests for standard

--- muscleUp/src/libs/standard.test.js
+++ muscleUp/src/libs/standard.test.js
@@ -1,4 +1,4 @@
-import { Exercise } from './standard'
+import { Exercise, Sets } from './standard'
 
 describe('Calculate 1RM', () => {
   it('Get 1Rm calculate with 80kg and 10 reps', () => {
@@ -17,12 +17,35 @@ describe('Kilograms per repetition', () => {
   it('Should give the kilograms per repetition', () => {
     const exercise = new Exercise(50, 5, 0, 3, 10)
     const kg = exercise.calculateKgForReps()
-    expect(kg).toBe(42.75)
+    expect(kg).toBe(42.8)
   })
 
   it('Should give the kilograms per repetition when complete all rates success', () => {
     const exercise = new Exercise(50, 5, 1, 3, 10)
     const kg = exercise.calculateKgForReps()
-    expect(kg).toBe(45.75)
+    expect(kg).toBe(45.8)
+  })
+})
+
+describe('Kilograms per sets', () => {
+  it('Should give the kilograms per sets', () => {
+    const exercise = new Exercise(50, 5, 0, 3, 10, 4)
+    const maxWeight = exercise.calculateKgForReps()
+    const minWeight = exercise.calculateMinWeightForReps()
+    console.log(maxWeight)
+    expect(maxWeight).toBe(42.8)
+    expect(minWeight).toBe(30)
+  })
+})
+
+describe('Sets per works', () => {
+  it('Should give total sets per work', () => {
+    const expected = [
+      { weight: '38.5', performance: 90, set: 2 },
+      { weight: '30.0', performance: 70, set: 4 },
+    ]
+    const sets = new Sets(50, 5, 0, 3, 10, 4)
+    const setsForExercise = sets.weightForSets()
+    expect(setsForExercise).toEqual(expect.arrayContaining(expected))
   })
 })
