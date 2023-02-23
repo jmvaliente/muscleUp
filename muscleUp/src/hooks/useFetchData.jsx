0426c2eccabf4d07c99045f70283275bcc00872e
jmvaliente <valiente_jose2@hotmail.com>
2023-02-23 12:10:44 +0100

    Check dev mode and set mock data

--- muscleUp/src/hooks/useFetchData.jsx
+++ muscleUp/src/hooks/useFetchData.jsx
@@ -1,26 +1,32 @@
 import { useState, useEffect } from 'react'
 import supabase from '../services/supabase'
+import { exercises } from '../services/mock/data'
 
 const useFetchData = () => {
   const [data, setData] = useState([])
   const [loading, setLoading] = useState(false)
   const [error, setError] = useState({})
+  const isDev = import.meta.env.VITE_ENVIRONMENT === 'DEV'
 
   useEffect(() => {
-    supabase.auth
-      .getUser()
-      .then(currentUser => {
-        setLoading(true)
-        supabase
-          .from('Exercise')
-          .select('*')
-          .eq('user_id', currentUser.data.user.id)
-          .then(response => {
-            setData(response.data)
-            setLoading(false)
-          })
-      })
-      .catch(responseError => setError(responseError))
+    if (isDev) {
+      setData(exercises)
+    } else {
+      supabase.auth
+        .getUser()
+        .then(currentUser => {
+          setLoading(true)
+          supabase
+            .from('Exercise')
+            .select('*')
+            .eq('user_id', currentUser.data.user.id)
+            .then(response => {
+              setData(response.data)
+              setLoading(false)
+            })
+        })
+        .catch(responseError => setError(responseError))
+    }
   }, [])
   return { data, error, loading }
 }
