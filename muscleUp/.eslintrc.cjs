dcb91c2fafed3ad6e1ad8cd6548bca684eca73c9
jmvaliente <valiente_jose2@hotmail.com>
2023-02-23 15:00:33 +0100

    format eslint file and add jest formmating rules

--- muscleUp/.eslintrc.cjs
+++ muscleUp/.eslintrc.cjs
@@ -1,26 +1,20 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  env: {
+    browser: true,
+    es2021: true,
+    'jest/globals': true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {},
+  settings: {
+    react: {
+      version: 'detect',
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
-    ],
-    "overrides": [
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-    },
-    "settings": {
-        "react": {
-          "version": "detect",
-        },
-      }
+  },
 }
