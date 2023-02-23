317dab0331b2db70e198be925763f3ef962fe589
jmvaliente <valiente_jose2@hotmail.com>
2023-02-23 12:14:29 +0100

    Omit login when stay in dev mode

--- muscleUp/src/App.jsx
+++ muscleUp/src/App.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Spinner, Button } from 'flowbite-react'
+
 import Navbar from './ui/components/Navbar'
 import Card from './ui/components/Card'
+import ModalRegister from './ui/modal/ModalRegister'
 import Register from './ui/components/Register'
 import supabase from './services/supabase'
 
@@ -8,22 +11,58 @@ import useFetchData from './hooks/useFetchData'
 import useGetUser from './hooks/useGetUser'
 
 function App() {
+  const [isVisible, setVisible] = useState(false)
   const user = useGetUser()
   const exercises = useFetchData()
+  const isDev = import.meta.env.VITE_ENVIRONMENT === 'DEV'
 
-  return (
-    <div>
-      <Navbar />
-      {user.data.data?.user ? (
-        <div className="flex flex-wrap justify-center">
+  const isLoadingUser = () =>
+    user.loading ? (
+      <div className="text-center">
+        <Spinner size="xl" />
+      </div>
+    ) : null
+
+  const isLoadingExercises = () =>
+    exercises.loading ? (
+      <div className="text-center">
+        <Spinner size="xl" />
+      </div>
+    ) : (
+      <>
+        <div className="flex flex-wrap justify-center self-center">
           {exercises.data.map(exercise => (
             <Card key={exercise.id}>{exercise}</Card>
           ))}
+          <div className="flex justify-center p-5">
+            <div className="rounded-xl shadow-lg bg-white max-w-sm">
+              <div className="justify-center flex items-center h-full">
+                <Button
+                  outline={true}
+                  gradientDuoTone="purpleToBlue"
+                  onClick={() => setVisible(true)}
+                >
+                  Add new Exercise
+                </Button>
+              </div>
+            </div>
+          </div>
         </div>
+      </>
+    )
+
+  return (
+    <div>
+      <ModalRegister isVisible={isVisible} setVisible={setVisible} />
+      {user.loading ? (
+        isLoadingUser()
       ) : (
-        <Register />
+        <div>
+          <Navbar />
+          {user.data.data?.user || isDev ? isLoadingExercises() : <Register />}
+          <button onClick={() => supabase.auth.signOut()}>Log out</button>
+        </div>
       )}
-      <button onClick={() => supabase.auth.signOut()}>Log out</button>
     </div>
   )
 }
