1aa9a80774d6d1f9e98c13343c8a9c5754587f15
jmvaliente <valiente_jose2@hotmail.com>
2023-02-24 01:06:15 +0100

    change props to card for new standards classes

--- muscleUp/src/ui/components/Card/enhancer.jsx
+++ muscleUp/src/ui/components/Card/enhancer.jsx
@@ -1,18 +1,32 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { calculateRateForSteps, calculate1Rm } from '../../../libs/standard'
+import { Exercise, Sets } from '../../../libs/standard'
 
 const enhance = View => {
   const Card = ({ children }) => {
     const { repsRm1, rm1, sets, reps, rate, stepsCompleted } = children
-    const rm1Real = calculate1Rm(rm1, repsRm1, rate, stepsCompleted)
-    const routine = calculateRateForSteps(
-      rm1Real,
-      sets,
-      reps,
-      rate,
+    const exercise = new Exercise(
+      rm1,
+      repsRm1,
       stepsCompleted,
+      rate,
+      reps,
+      sets,
     )
+    const setsForExercise = new Sets(
+      rm1,
+      repsRm1,
+      stepsCompleted,
+      rate,
+      reps,
+      sets,
+    )
+
+    const routine = {
+      maxWeight: exercise.calculateKgForReps(),
+      minWeight: exercise.calculateMinWeightForReps(),
+      weightForSets: setsForExercise.weightForSets(),
+    }
     return <View data={children} routine={routine} />
   }
 
