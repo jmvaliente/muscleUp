d59e5342ec86702fc234c5a6fc5b755371c9c4ed
jmvaliente <valiente_jose2@hotmail.com>
2023-02-24 11:58:23 +0100

    refactor to Domain data

--- muscleUp/src/ui/components/Card/enhancer.jsx
+++ muscleUp/src/ui/components/Card/enhancer.jsx
@@ -4,23 +4,8 @@ import { Exercise, Sets } from '../../../libs/standard'
 
 const enhance = View => {
   const Card = ({ children }) => {
-    const { repsRm1, rm1, sets, reps, rate, stepsCompleted } = children
-    const exercise = new Exercise(
-      rm1,
-      repsRm1,
-      stepsCompleted,
-      rate,
-      reps,
-      sets,
-    )
-    const setsForExercise = new Sets(
-      rm1,
-      repsRm1,
-      stepsCompleted,
-      rate,
-      reps,
-      sets,
-    )
+    const exercise = new Exercise(children)
+    const setsForExercise = new Sets(children)
 
     const routine = {
       maxWeight: exercise.calculateKgForReps(),
